4fd383bf50723c77e67cfcf83151d26eee681c11
rajpal229 <rajpalrawat229@gmail.com>
2022-12-14 19:55:41 +0530

    Added: UC6_MaximumHours

--- EmpWage.cs
+++ EmpWage.cs
@@ -17,31 +17,41 @@ namespace EmployeWageComputationProblem_Part3
             int empPartTime = 2;
             int perHour = 20;
             int maxDaysInMonth = 20;
-            int empHrs = 0, dailyWage = 0, monthlyWage = 0;
-            for (int days = 1; days <= maxDaysInMonth; days++)
+            int empHrs = 0, dailyWage = 0, monthlyWage = 0, maxHr = 100, totalEmpHrs = 0 ;
+            for (int days = 1; days <= maxDaysInMonth || totalEmpHrs <=100; days++)
             {
                 Random random = new Random();
-            int check = random.Next(0, 2);
-            switch (check)
-            {
-                case 1:
-                    Console.WriteLine("Employee is Present Full-Time");
-                    empHrs = 8;
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is Present Part-Time");
-                        empHrs = 4;
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case 1:
+                        Console.WriteLine("Employee is Present Full-Time");
+                        empHrs = 8;
                         break;
-                default:
-                        empHrs = 0;
-                        Console.WriteLine("Employee is Absent");
+                    case 2:
+                        Console.WriteLine("Employee is Present Part-Time");
+                            empHrs = 4;
+                            break;
+                    default:
+                            empHrs = 0;
+                            Console.WriteLine("Employee is Absent");
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                if (totalEmpHrs < maxHr)
+                {
+                    dailyWage = empHrs * perHour;
+                    monthlyWage += dailyWage;
+                    Console.WriteLine("employee daily wage:" + dailyWage);
+                }
+                else
+                {
+                    totalEmpHrs -= empHrs;
                     break;
+                }
             }
-                dailyWage = empHrs * perHour;
-                 monthlyWage += dailyWage;
-                Console.WriteLine("employee wage:" + dailyWage);
-            }
-            Console.WriteLine("total employee wage:" + monthlyWage);
+            Console.WriteLine("Total Working Hours:" + totalEmpHrs);
+            Console.WriteLine("monthly employee wage:" + monthlyWage);
 
         }
     }
