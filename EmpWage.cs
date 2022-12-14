d4f4b3458776a634672d1db2e537e720eef12fc8
rajpal229 <rajpalrawat229@gmail.com>
2022-12-14 19:21:05 +0530

    Added: UC2_DailyWage

--- EmpWage.cs
+++ EmpWage.cs
@@ -12,12 +12,15 @@ namespace EmployeWageComputationProblem_Part3
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             int empPresent = 1;
+            int perHour = 20;
+            int fullTime = 8;
             Random random = new Random();
             int check = random.Next(0, 2);
             if (check == empPresent)
             {
                 Console.WriteLine("Employee is Present");
-
+                int perDay = perHour * fullTime;
+                Console.WriteLine("Daily wadge : " + perDay);
             }
             else
             {
